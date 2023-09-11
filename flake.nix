ee027cd04b449a273060ab5d48ec91899a9634b8
Viktor Kronvall <viktor.kronvall@gmail.com>
2023-09-11 15:32:09 +0900

    try without mounting proc

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # The nixpkgs
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:considerate/nixpkgs/considerate/no-proot-proc";
 
     # Utility functions
     flake-utils.url = "github:numtide/flake-utils";
